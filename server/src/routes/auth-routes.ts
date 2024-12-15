5e1ab6e96e1c252d0a23963e2340edd7faa710dd
jsparrowio <jsparrowio@outlook.com>
2024-12-15 12:23:55 -0800

    finished server side user auth

--- server/src/routes/auth-routes.ts
+++ server/src/routes/auth-routes.ts
@@ -4,12 +4,29 @@ import jwt from 'jsonwebtoken';
 import bcrypt from 'bcrypt';
 
 export const login = async (req: Request, res: Response) => {
-  // TODO: If the user exists and the password is correct, return a JWT token
+
+const { username, password } = req.body;
+
+const user = await User.findOne({
+  where: { username },
+});
+if(!user) {
+  return res.status(401).json({ message: 'User not found. Did the user mean to sign up?' });
+}
+
+const validatePassword = await bcrypt.compare(password, user.password);
+if (!validatePassword) {
+  return res.status(401).json({ message: 'Username or Password incorrect' });
+}
+
+const secretKey = process.env.JWT_SECRET_KEY || '';
+
+const token = jwt.sign({ username }, secretKey, { expiresIn: '1hr' });
+return res.json({ token })
 };
 
 const router = Router();
 
-// POST /login - Login a user
 router.post('/login', login);
 
 export default router;
